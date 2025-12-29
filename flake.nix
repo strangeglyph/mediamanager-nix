390a37d9ff89a0814ef82bd4e5d7c8a37e5e9cd7
lschuetze <lschuetze@mpi-sws.org>
2025-12-29 18:37:09 +0100

    initial setup
    test script

---
+++ flake.nix
@@ -0,0 +1,157 @@
+{
+  description = "Package MediaManager for nix consumption";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    media-manager = {
+      url = "github:maxdorninger/MediaManager";
+      flake = false;
+    };
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      media-manager,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = media-manager; };
+
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      overrides = final: prev:
+        let
+          inherit (final) resolveBuildSystem;
+          inherit (builtins) mapAttrs;
+          buildSystemOverrides = {
+            "bencoder".setuptools = [];
+          };
+        in
+        mapAttrs (
+          name: spec:
+          prev.${name}.overrideAttrs (old: {
+            nativeBuildInputs = old.nativeBuildInputs ++ resolveBuildSystem spec;
+          })
+        ) buildSystemOverrides;
+
+      pythonSets = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          python = lib.head (pyproject-nix.lib.util.filterPythonInterpreters {
+            inherit (workspace) requires-python;
+            inherit (pkgs) pythonInterpreters;
+          });
+        in
+        (pkgs.callPackage pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+          (
+            lib.composeManyExtensions [
+              pyproject-build-systems.overlays.wheel
+              overlay
+              overrides
+            ]
+          )
+      );
+
+    in
+    {
+      packages = forAllSystems (system: 
+        let
+          pythonSet = pythonSets.${system};
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (pkgs.callPackages pyproject-nix.build.util { }) mkApplication;
+        in
+        {
+          virtual-env = (pythonSet.mkVirtualEnv "media-manager-env" workspace.deps.default)
+                          .overrideAttrs (old: {
+                            venvIgnoreCollisions = old.venvIgnoreCollisions ++ [ "*/fastapi" ];
+                          });
+          # TODO this is an awful hack 
+          application = 
+            let 
+              venv = self.outputs.packages."${system}".virtual-env; 
+            in pkgs.writeShellScriptBin "mediamanager" ''
+              set -euo pipefail
+              
+              source ${venv}/bin/activate
+              
+              (cd ${media-manager} && python -m alembic upgrade head)
+              python -m fastapi run ${venv}/lib/python3.13/site-packages/media_manager/main.py $@ 
+            '';
+          default = self.outputs.packages."${system}".application;
+        }
+      );
+
+      overlays.default = final: prev: {
+        media-manager = self.outputs.packages."${prev.stdenv.hostPlatform.system}".application;
+      };
+
+      modules.default = {
+        imports = [ ./module.nix ];
+        #config.nixpkgs.overlays = [ self.outputs.overlays.default ];
+      };
+
+      checks = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          {
+            default = pkgs.testers.runNixOSTest {
+              name = "mediamanager integration test";
+              nodes.machine = { config, pkgs, ...}: {
+                imports = [ self.outputs.modules.default ];
+                config = {
+                  services.media-manager = {
+                    enable = true;
+                    package = self.outputs.packages."${system}".application;
+                    postgres.enable = true;
+                    dataDir = "/tmp"; 
+                  };
+                  
+                  systemd.tmpfiles.settings."10-mediamanager" = {
+                    "/tmp/movies".d = { user = config.services.media-manager.user; };
+                    "/tmp/shows".d = { user = config.services.media-manager.user; };
+                    "/tmp/images".d = { user = config.services.media-manager.user; };
+                    "/tmp/torrents".d = { user = config.services.media-manager.user; };
+                  };
+                };
+              };
+              testScript = { nodes, ... }: ''
+                machine.wait_for_unit("media-manager.service")
+              '';
+            };
+          }
+      );
+    };
+}
---
+++ module.nix
@@ -0,0 +1,219 @@
+{ pkgs, lib, config, ... }:
+let
+  inherit (lib) 
+    mkOption
+    mkPackageOption
+    mkEnableOption
+    mkIf
+    types;
+  cfg = config.services.media-manager;
+
+  settings-format = pkgs.formats.toml {};
+  settings-file = settings-format.generate "config.toml" cfg.settings;
+in
+{
+  options = {
+    services.media-manager = {
+      enable = mkEnableOption { 
+        description = "MediaManager"; 
+      };
+      
+      package = mkPackageOption pkgs "MediaManager" { 
+        #default = pkgs.media-manager;
+      };
+      
+      user = mkOption {
+        description = "The user to run MediaManager as";
+        type = types.str;
+        default = "media-manager";
+      };
+
+      group = mkOption {
+        description = "The group to run MediaManager as";
+        type = types.str;
+        default = "media-manager";
+      };
+
+      dataDir = mkOption {
+        description = "Directory where MediaManager manages files";
+        type = types.path;
+      };
+
+      host = mkOption {
+        description = "IP address to bind to";
+        type = types.str;
+        default = "::1";
+        example = "0.0.0.0";
+      };
+
+      port = mkOption {
+        description = "Port number to bind to";
+        type = types.port;
+        default = 8000;
+      };
+
+      root-path = mkOption {
+        description = "If MediaManager is served under a sub path (e.g. https://example.com/mediamanager), set it here";
+        type = types.nullOr types.str;
+        default = null;
+        example = "/mediamanager";
+      };
+
+      environmentFile = mkOption {
+        description = ''
+          Path to file storing environment variables to be passed to the service.
+
+          See https://maxdorninger.github.io/MediaManager/configuration.html#configuring-secrets for details on the
+          variable naming.
+        '';
+        type = types.nullOr types.path;
+        default = null;
+        example = "/run/secrets/media-manager.env";
+      };
+
+      settings = mkOption {
+        description = ''
+          Settings for MediaManager.
+
+          See https://maxdorninger.github.io/MediaManager/configuration.html for details.
+        '';
+        default = {};
+        type = types.submodule {
+          freeformType = settings-format.type;
+          options = {
+            misc = {
+              image_directory = mkOption {
+                type = types.path;
+                default = "${cfg.dataDir}/images";
+                defaultText = "\${cfg.dataDir}/images";
+              };
+              tv_directory = mkOption {
+                type = types.path;
+                default = "${cfg.dataDir}/tv";
+                defaultText = "\${cfg.dataDir}/tv";
+              };
+              movie_directory = mkOption {
+                type = types.path;
+                default = "${cfg.dataDir}/movies";
+                defaultText = "\${cfg.dataDir}/movies";
+              };
+              torrent_directory = mkOption {
+                type = types.path;
+                default = "${cfg.dataDir}/torrents";
+                defaultText = "\${cfg.dataDir}/torrents";
+              };
+            };
+            database = {
+              host = mkOption {
+                description = "Postgres hostname. Leave empty for unix socket";
+                type = types.str;
+                default = "";
+                example = "remote-db.example.com";
+              };
+              user = mkOption {
+                description = "Username to use for postgres access";
+                type = types.str;
+                default = cfg.postgres.user;
+                defaultText = "`cfg.postgres.user`";
+              };
+              dbname = mkOption {
+                description = "Database name for MediaManager";
+                type = types.str;
+                default = cfg.postgres.user;
+                defaultText = "`cfg.postgres.user`";
+              };
+            };
+          };
+        };
+      };
+
+      postgres = {
+        enable = mkEnableOption {
+          description = "Whether to configure Postgres for MediaManager";
+        };
+        user = mkOption {
+          description = ''
+            Postgres database user. Note that if this differs from the MediaManager
+            service user, you need to manually set up a user mapping. See:
+            https://nixos.org/manual/nixos/stable/#module-services-postgres-authentication
+          '';
+          type = types.str;
+          default = cfg.user;
+          defaultText = "\${cfg.user}";
+        };
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    users.users = lib.optionalAttrs (cfg.user == "media-manager") {
+      media-manager = {
+        isSystemUser = true;
+        group = cfg.group;
+      };
+    };
+
+    users.groups = lib.optionalAttrs (cfg.group == "media-manager") { 
+      media-manager = {};
+    };
+
+    systemd.services."media-manager" = {
+      description = "the MediaManager service";
+
+      wantedBy = [ "multi-user.target" ];
+      requires = [ "postgresql.service" ];
+      after = [ "network.target" "postgresql.service" ];
+      enable = true;
+
+      enableStrictShellChecks = true;
+
+      environment = {
+        CONFIG_FILE = settings-file;
+        LOG_FILE = "/var/log/media-manager/log.json";
+      };
+
+      script = ''
+        test -v MEDIAMANAGER_AUTH__TOKEN_SECRET \
+          || MEDIAMANAGER_AUTH__TOKEN_SECRET=$(${lib.getExe pkgs.openssl} rand -hex 64)
+
+        export MEDIAMANAGER_AUTH__TOKEN_SECRET
+        
+        ${lib.getExe cfg.package} \
+          --host ${cfg.host} \
+          --port ${toString cfg.port} \
+          --proxy-headers \
+          ${lib.optionalString (cfg.root-path != null) cfg.root-path}
+      '';
+      
+      unitConfig = {
+        RequiresMountsFor = [
+          cfg.settings.misc.movie_directory
+          cfg.settings.misc.tv_directory
+          cfg.settings.misc.image_directory
+          cfg.settings.misc.torrent_directory
+        ];
+      };
+
+      serviceConfig = {
+        LogsDirectory = "media-manager";
+        StateDirectory = "media-manager";
+        EnvironmentFile = lib.optional (cfg.environmentFile != null) cfg.environmentFile;
+        WorkingDirectory = "/var/lib/media-manager";
+        User = cfg.user;
+        Group = cfg.group;
+        Restart = "always";
+      };
+    };
+
+    services.postgresql = mkIf cfg.postgres.enable {
+      enable = true;
+      ensureDatabases = [ cfg.postgres.user ]; # keep name consistent
+      ensureUsers = [
+        {
+          name = cfg.postgres.user;
+          ensureDBOwnership = true;
+        }
+      ];
+    };
+  };
+}
